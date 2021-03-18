7cf5096646ac6707a345a4ded30cf465d1709354
Erik Dubois <erik.dubois@gmail.com>
2021-03-18 11:41:49 +0100

    logo smaller

--- logos/arch.h
+++ logos/arch.h
@@ -1,45 +1,22 @@
-// This was stolen from neofetch
-// char *LOGO[] = {
-//     "                  -`                    ",
-//     "                 .o+`                   ",
-//     "                `ooo/                   ",
-//     "               `+oooo:                  ",
-//     "              `+oooooo:                 ",
-//     "              -+oooooo+:                ",
-//     "            `/:-:++oooo+:               ",
-//     "           `/++++/+++++++:              ",
-//     "          `/++++++++++++++:             ",
-//     "         `/+++ooooooooooooo/`           ",
-//     "        ./ooosssso++osssssso+`          ",
-//     "       .oossssso-````/ossssss+`         ",
-//     "      -osssssso.      :ssssssso.        ",
-//     "     :osssssss/        osssso+++.       ",
-//     "    /ossssssss/        +ssssooo/-       ",
-//     "  `/ossssso+/:-        -:/+osssso+-     ",
-//     " `+sso+:-`                 `.-/+oso:    ",
-//     "`++:.                           `-/+/   ",
-//     ".`                                 `/   "
-// };
-
 char *LOGO[] = {
-	"                    /-                    "
-    "                   ooo:                   "
-    "                  yoooo/                  " 
-    "                 yooooooo                 " 
-    "                yooooooooo                "
-    "               yooooooooooo               "
-    "             .yooooooooooooo              "
-    "            .oooooooooooooooo             "
-    "           .oooooooarcoooooooo            "
-    "          .ooooooooo-oooooooooo           "
-    "         .ooooooooo-  oooooooooo          "
-    "        :ooooooooo.    :ooooooooo         "
-    "       :ooooooooo.      :ooooooooo        "
-    "      :oooarcooo         .oooarcooo       "
-    "     :ooooooooy           .ooooooooo      "
-    "    :ooooooooo   ooooooooooooooooooo$     "
-   	"   :ooooooooo      .-ooooooooooooooooo    "
-  	"  ooooooooo-            -ooooooooooooo.   "
- 	" ooooooooo-                 -oooooooooo.  "
-	"ooooooooo.                    -ooooooooo  "
+	"                                        ",
+    "                   oo:                  ",
+    "                  yoo/                  ",
+    "                 yooooo                 ",
+    "                yooooooo                ",
+    "               yooooooooo               ",
+    "             .yooooooooooo              ",
+    "            .oooooooooooooo             ",
+    "           .ooooooarcooooooo            ",
+    "          .oooooooo-ooooooooo           ",
+    "         .oooooooo-  ooooooooo          ",
+    "        :oooooooo.    :oooooooo         ",
+    "       :oooooooo.      :oooooooo        ",
+    "      :ooarcooo         .oooarcoo       ",
+    "     :oooooooy           .oooooooo      ",
+    "    :oooooooo   oooooooooooooooooo$     ",
+   	"   :oooooooo      .-oooooooooooooooo    ",
+  	"  oooooooo-            -oooooooooooo.   ",
+ 	" oooooooo-                 -ooooooooo.  ",
+	"oooooooo.                    -oooooooo  "
 };
