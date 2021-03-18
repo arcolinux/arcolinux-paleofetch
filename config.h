f871479a11744ba2e285b559ccaefb193f7e1e50
Erik Dubois <erik.dubois@gmail.com>
2021-03-18 13:41:25 +0100

    small logo with less stuf

--- config.h
+++ config.h
@@ -4,17 +4,12 @@
 #define CONFIG \
 { \
    /* name            function                 cached */\
-    { "",             get_title,               false }, \
-    { "",             get_bar,                 false }, \
-    { "Host: ",       get_host,                true  }, \
     { "Kernel: ",     get_kernel,              true  }, \
     { "Uptime: ",     get_uptime,              false }, \
-    SPACER \
     { "Packages: ",   get_packages_pacman,   false }, \
     { "Shell: ",      get_shell,             false }, \
     { "Resolution: ", get_resolution,        false }, \
     { "Terminal: ",   get_terminal,          false }, \
-    SPACER \
     { "CPU: ",        get_cpu,                 true  }, \
     { "GPU: ",        get_gpu1,                true  }, \
     { "Memory: ",     get_memory,              false }, \
---
+++ logos/arco-big-small.h
@@ -0,0 +1,21 @@
+char *LOGO[] = {
+	"                                     ",
+    "                 o                   ",
+    "                ooo                  ",
+    "               ooooo                 ",
+    "              ooooooo                ",
+    "             ooooooooo               ",
+    "            ooooo ooooo              ",
+    "           ooooo   ooooo             ",
+    "          ooooo     ooooo            ",
+    "         ooooo       ooooo           ",
+    "        ooooo         ooooo          ",
+    "       ooooo           ooooo         ",
+    "      ooooo             ooooo        ",
+    "     ooooo               ooooo       ",
+    "    ooooo       ooooooooooooooo      ",
+   	"   ooooo           ooooooooooooo     ",
+  	"  ooooo                oooooooooo    ",
+ 	" ooooo                     ooooooo   ",
+	"ooooo                         ooooo  "
+};
