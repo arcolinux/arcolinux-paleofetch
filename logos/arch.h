5cbfac2aeeeaea5022ea5d33e9f1d858e6795b17
Erik Dubois <erik.dubois@gmail.com>
2021-03-18 10:25:22 +0100

    first commit

---
+++ logos/arch.h
@@ -0,0 +1,45 @@
+// This was stolen from neofetch
+// char *LOGO[] = {
+//     "                  -`                    ",
+//     "                 .o+`                   ",
+//     "                `ooo/                   ",
+//     "               `+oooo:                  ",
+//     "              `+oooooo:                 ",
+//     "              -+oooooo+:                ",
+//     "            `/:-:++oooo+:               ",
+//     "           `/++++/+++++++:              ",
+//     "          `/++++++++++++++:             ",
+//     "         `/+++ooooooooooooo/`           ",
+//     "        ./ooosssso++osssssso+`          ",
+//     "       .oossssso-````/ossssss+`         ",
+//     "      -osssssso.      :ssssssso.        ",
+//     "     :osssssss/        osssso+++.       ",
+//     "    /ossssssss/        +ssssooo/-       ",
+//     "  `/ossssso+/:-        -:/+osssso+-     ",
+//     " `+sso+:-`                 `.-/+oso:    ",
+//     "`++:.                           `-/+/   ",
+//     ".`                                 `/   "
+// };
+
+char *LOGO[] = {
+	"                    /-                    "
+    "                   ooo:                   "
+    "                  yoooo/                  " 
+    "                 yooooooo                 " 
+    "                yooooooooo                "
+    "               yooooooooooo               "
+    "             .yooooooooooooo              "
+    "            .oooooooooooooooo             "
+    "           .oooooooarcoooooooo            "
+    "          .ooooooooo-oooooooooo           "
+    "         .ooooooooo-  oooooooooo          "
+    "        :ooooooooo.    :ooooooooo         "
+    "       :ooooooooo.      :ooooooooo        "
+    "      :oooarcooo         .oooarcooo       "
+    "     :ooooooooy           .ooooooooo      "
+    "    :ooooooooo   ooooooooooooooooooo$     "
+   	"   :ooooooooo      .-ooooooooooooooooo    "
+  	"  ooooooooo-            -ooooooooooooo.   "
+ 	" ooooooooo-                 -oooooooooo.  "
+	"ooooooooo.                    -ooooooooo  "
+};
